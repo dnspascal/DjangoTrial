2b1c2166358e95da66ce53e879c98983107d3a33
dnspascal <102286019+dnspascal@users.noreply.github.com>
2023-05-24 18:49:25 +0300

    pushing in the branch

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 def homepageview(request):
-    return HttpResponse("Hello, world")
+    return HttpResponse("Hello, world now")
 
 class HomePageView(TemplateView):
     template_name = "home.html"
